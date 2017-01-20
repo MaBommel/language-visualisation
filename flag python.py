e071672d7a429bf1eafb96132ec2c6f1f1a07aea
MaBommel <marenberth@googlemail.com>
2017-01-20 21:24:35 +0100

    comments and writing
    I added some comments to describe the code. Besides, I added wirtings
    saying ech language name to the turtle window.

--- flag python.py
+++ flag python.py
@@ -2,6 +2,8 @@ import turtle
 from tkinter import *
 import time
 
+#starting with defining the functions that are supposed to show the flags later
+
 #belgium - gets its own function because it is going to be used more thanc once
 def belgiumflag():	
 	belgium = turtle.Turtle()
@@ -83,7 +85,14 @@ def southafriflag():
 def danishflag():
 	wn = turtle.Screen()     
 	turtle.title("Flags of countries in which Danish is spoken")   
-	denmark = turtle.Turtle()      
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Danish", font=("Arial",20, "bold"))
+	title.ht()
+	denmark = turtle.Turtle()
+	denmark.speed(10)
+	denmark.goto(0,0)
 	denmark.color("red")
 	denmark.pensize(200)
 	denmark.forward(150)
@@ -103,9 +112,14 @@ def danishflag():
 def dutchflags():
 	wn = turtle.Screen()
 	turtle.title("Flags of countries in which Dutch is spoken")
-	
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Dutch", font=("Arial",20, "bold"))
+	title.ht()	
 	#holland
 	holland = turtle.Turtle() 
+	holland.speed(10)
 	holland.color("red")
 	holland.pensize(50)
 	holland.goto(-200,0)
@@ -123,7 +137,11 @@ def dutchflags():
 def germanflags():
 	wn = turtle.Screen() 
 	turtle.title("Flags of countries in which German is spoken")
-
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("German", font=("Arial",20, "bold"))
+	title.ht()
 	#germany
 	germany = turtle.Turtle()      
 	germany.speed(10)
@@ -187,7 +205,11 @@ def germanflags():
 def englishflags():
 	wn = turtle.Screen()
 	turtle.title("Flags of some countries in which English is spoken")
-
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("English (Some Examples)", font=("Arial",20, "bold"))
+	title.ht()
 	#uk
 	uk = turtle.Turtle()
 	uk.speed(10)
@@ -260,6 +282,7 @@ def englishflags():
 		us.left(90)
 		us.forward(50)
 		us.left(90)
+
 	#canada
 	canada = turtle.Turtle()
 	canada.speed(10)
@@ -290,8 +313,14 @@ def englishflags():
 
 def swedenflag():
 	wn = turtle.Screen()   
-	turtle.title("Flags of countries in which Swedish is spoken")     
-	sweden = turtle.Turtle()      
+	turtle.title("Flags of countries in which Swedish is spoken")  
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Swedish", font=("Arial",20, "bold"))   
+	title.ht()
+	sweden = turtle.Turtle()    
+	sweden.speed(10)  
 	sweden.color("blue")
 	sweden.pensize(200)
 	sweden.forward(150)
@@ -311,6 +340,11 @@ def swedenflag():
 def norwayflag():
 	wn = turtle.Screen()
 	turtle.title("Flags of countries in which Norwegian is spoken")
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Norwegian", font=("Arial",20, "bold"))
+	title.ht()
 	norway = turtle.Turtle()      
 	norway.speed(10)
 	norway.color("red")
@@ -338,8 +372,14 @@ def norwayflag():
 
 def icelandflag():
 	wn = turtle.Screen() 
-	turtle.title("Flags of countries in which Icelandic is spoken")       
+	turtle.title("Flags of countries in which Icelandic is spoken")
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Icelandic", font=("Arial",20, "bold"))
+	title.ht()
 	iceland = turtle.Turtle()
+	iceland.speed(10)
 	iceland.color("blue")
 	iceland.pensize(200)
 	iceland.forward(150)
@@ -365,8 +405,12 @@ def icelandflag():
 
 def afrikaansflag():
 	wn = turtle.Screen()
-	turtle.title("Flags of countries in which Afrikaans is spoken") 
-
+	turtle.title("Flags of countries in which Afrikaans is spoken")
+	title = turtle.Turtle()
+	title.up()
+	title.goto(-50,270)
+	title.write("Afrikaans", font=("Arial",20, "bold"))
+	title.ht()
 	#namibia
 	nami = turtle.Turtle()
 	nami.speed(10)
@@ -404,7 +448,7 @@ def afrikaansflag():
 	#south africa
 	southafriflag()
 
-	#window that is supposed to open for a false entry
+# defining a function for a window that is supposed to open for a false entry
 
 def falseentry():
 	wn = turtle.Screen()
@@ -416,27 +460,38 @@ def falseentry():
 	false.write("False Entry!", font=("Arial", 33, "bold"))
 	time.sleep(3)
 
-danishfam = ["moder", "far", "bedstemor", "bedstefar", "onkel", "tante",
- "datter", "søn", "søster", "bror", "nevø", "niece", "barnebarn", "fætter"]
-dutchfam = ["moeder", "vader", "grootmoeder", "grootvader", "oom", "tante",
- "dochter", "zoon", "zus", "broer", "neef", "nicht", "kleinzoon", "kleindochter"]
+#listing the expressions for family members to each language that is represented as flags as well
+
+danishfam = ["moder", "far", "bedstemor", "bedstefar", "onkel", "tante"
+"datter", "søn", "søster", "bror", "nevø", "niece", "barnebarn", "barnebarn"
+ "fætter", "fætter"]
+dutchfam = ["moeder", "vader", "grootmoeder", "opa", "oom", "tante"
+"dochter", "zoon", "zuster", "broer", "neef", "nicht", "kleinzoon", 
+"kleindochter", "neef", "neef"]
 germanfam = ["Mutter", "Vater", "Großmutter", "Großvater", "Onkel", "Tante",
 "Tochter", "Sohn", "Schwester", "Bruder", "Neffe", "Nichte", "Enkel", "Enkelin",
  "Cousin", "Cousine"]
-englishfam = ["mother", "father", "grandpa", "grandma", "uncle", "aunt",
- "daughter", "sister", "brother", "son", "niece", "nephew", "grandchild",
-  "cousin"]
-swedishfam = ["mamma", "fader", "mormor", "farfar", "farbror", "moster", "dotter",
- "son", "syster", "broder", "brorson", "niece", "barnbarn", "kusin"]
-norfam = ["mother", "far", "bestemor", "bestefar", "onkel", "tante", "datter",
- "sønn", "søster", "bror", "nevø", "niese", "barnebarn", "fetter"]
-icefam = ["móðir", "faðir", "amma", "afi", "frændi", "frænka", "dóttir",
- "sonur", "systir", "bróðir", "frændi", "frænka", "barnabarn","frændi"]
-afrikaansfam = ["moeder", "vader", "ouma", "Oupa", "oom", "tannie", "dogter",
- "seun", "suster", "broer", "neef", "niggie", "kleinseun","kleindogter","neef",
- "neef"]
+englishfam = ["mother", "father", "grandmother", "grandfather", "uncle", "aunt"
+"daughter", "son", "sister", "brother", "nephew", "niece", "grandson", 
+"granddaughter", "cousin, cousin"]
+swedishfam = ["mamma", "fader", "mormor", "farfar", "farbror", "moster"
+"dotter", "son", "syster", "broder", "brorson", "niece", "barnbarn", "barnbarn"
+ "kusin", "kusin"]
+norfam = ["mother", "far", "bestemor", "bestefar", "onkel", "tante"
+"daughter", "sønn", "søster", "bror", "nevø", "niese", "barnebarn", 
+"barnebarn", "fetter", "fetter"]
+icefam = ["móðir", "faðir", "amma", "Afi", "frændi", "frænka", "dóttir",
+ "sonur", "systir", "bróðir", "frændi", "frænka", "barnabarn", "barnabarn"
+ "cousin," "frændi"]
+afrikaansfam = ["moeder", "vader", "ouma", "oupa", "oom", "tannie", "dogter",
+ "seun", "suster", "broer", "neef", "niggie", "kleinseun", "kleindogter"
+ "neef," "neef"]
+
+#list to which the word typed in by the user is going to be added
 
 insert_word = []
+
+#defining a function for a user interface, pattern for interface found on http://www.python-course.eu/tkinter_entry_widgets.php
 
 def show_entry_fields():
    insert_word.append(e1.get())
@@ -455,6 +510,8 @@ Button(master, text='2. Step: Show flag', command=master.quit).grid(row=3,
  column=0, sticky=W, pady=4)
 
 mainloop()
+
+#defining a final function to connect flag functions with language lists
 	
 def langvisual():
 	for i in insert_word:
